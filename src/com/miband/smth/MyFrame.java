6ecd737463574e9ec153054f998ad338d473d260
cmiband <bartadamo2005@gmail.com>
2021-10-27 18:56:51 +0200

    head evaporates

--- src/com/miband/smth/MyFrame.java
+++ src/com/miband/smth/MyFrame.java
@@ -19,7 +19,11 @@ public class MyFrame extends JFrame implements KeyListener{
 	private JPanel image;
 	private JLabel answer;
 	private JLabel word;
+	private JLabel winScreen;
+	private JLabel loseScreen;
 	private String randomWord;
+	private String hiddenWord;
+	private int misses;
 	
 	public MyFrame() {
 		super("Hangman");
@@ -31,6 +35,7 @@ public class MyFrame extends JFrame implements KeyListener{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 300);
 		addKeyListener(this);
+		misses = 0;
 		
 		setBackground(Color.white);
 		setResizable(false);
@@ -47,7 +52,8 @@ public class MyFrame extends JFrame implements KeyListener{
 		word = new JLabel("");
 		
 		randomWord = randomizer.getRandomWord();
-		String hiddenWord = hide(randomWord);
+		hiddenWord = hide(randomWord);
+		System.out.println(randomWord);
 		word = new JLabel(hiddenWord);
 			
 		game.add(image);
@@ -64,17 +70,25 @@ public class MyFrame extends JFrame implements KeyListener{
 		return hidden;
 	}
 	
-	void unHideCharacter(char c, String word, String hiddenWord) {
+	String UnhideCharacter(char c, String word, String previous) {
 		char[] signs = new char[word.length()];
+		char[] hiddenSigns = new char[previous.length()];
 		for(int i = 0; i<word.length(); i++) {
 			signs[i] = word.charAt(i);
+			hiddenSigns[i] = previous.charAt(i);
 		}
 		
+		String r = "";
 		for(int i = 0; i<word.length(); i++) {
 			if(c == signs[i]) {
-				
+				r += c;
 			}
+			if(hiddenSigns[i]=='*'){
+				r += '*';
+			}
+
 		}
+		return r;
 	}
 	
 	boolean isInWord(char c, String word) {
@@ -90,6 +104,22 @@ public class MyFrame extends JFrame implements KeyListener{
 		}
 		return false;
 	}
+	
+	boolean win(String word) {
+		char[] signs = new char[word.length()];
+		for(int i = 0; i<word.length(); i++) {
+			if(signs[i]=='*') {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	boolean lose() {
+		if(misses >= 8)
+			return true;
+		return false;
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -100,9 +130,25 @@ public class MyFrame extends JFrame implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		char c = e.getKeyChar();
+		System.out.println(c);
 		boolean inWord = isInWord(c, randomWord);
 		if(inWord) {
-			
+			String uc = UnhideCharacter(c, randomWord, hiddenWord);
+			hiddenWord = uc;
+			System.out.println(hiddenWord);
+			word = new JLabel(hiddenWord);
+			if(win(uc)) {
+				winScreen = new JLabel("You won!");
+				this.add(winScreen);
+			}
+		}
+		else {
+			misses++;
+			if(lose()) {
+				loseScreen = new JLabel("You lose!");
+				this.add(loseScreen);
+			}
+				
 		}
 		
 	}
